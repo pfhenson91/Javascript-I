408a8f7f40b77341c2548336589f5604be394533
Preston Henson <pfhenson91@gmail.com>
2016-02-24 19:07:04 -0800

    initial

---
+++ day1.js
@@ -0,0 +1,17 @@
+// Geekwise KSI - Day 1 Notes
+// Preston Henson
+// 2/22/16
+"use strict";
+
+var favFood = "Ice Cream";
+var favDrink = "Southern Comfort";
+
+var favFoodAndDrink = favFood + ", " + favDrink;
+
+console.log(favFoodAndDrink);
+
+var myAge = 24;
+var oneDecade = 10;
+var myAgeInADecade = myAge + oneDecade;
+console.log(myAgeInADecade);
+
